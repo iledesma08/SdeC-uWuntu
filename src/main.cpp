f02a64bf329a6fe3d112996ebe5ea2803b9e18b6
iledesma <ignacioledesmachiappero@gmail.com>
2025-03-22 15:17:25 -0300

    changed ESP32 code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
 
+#define CPU_MHZ 80
+#define SERIAL_BAUDRATE 115200
+
 // Configurable: cantidad de iteraciones
-const int loop_count = 10000000;
+const int loop_count = 100000;
 
 // Función con sumas de enteros
 void suma_enteros() {
   volatile int acc = 0;
   for (int i = 0; i < 100; i++) {
-    acc += i;
+    acc += rand() % 100;
   }
 }
 
@@ -15,16 +18,16 @@ void suma_enteros() {
 void suma_floats() {
   volatile float acc = 0;
   for (int i = 0; i < 100; i++) {
-    acc += i * 0.1f;
+    acc += (rand() % 100)/rand();
   }
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(SERIAL_BAUDRATE);
   delay(1000);  // Esperar que el monitor serial conecte
 
   // Cambiar la frecuencia del CPU (80, 160, 240 MHz)
-  setCpuFrequencyMhz(160);  // Cambiá este valor para probar
+  setCpuFrequencyMhz(CPU_MHZ);  // Cambiá este valor para probar
   delay(100);
 
   Serial.printf("Frecuencia actual del CPU: %d MHz\n", getCpuFrequencyMhz());
@@ -33,7 +36,7 @@ void setup() {
 
   for (int i = 0; i < loop_count; i++) {
     suma_enteros();
-    suma_floats();
+    //suma_floats();
   }
 
   unsigned long end = millis();
