388c7b559e1e10d45bc905ddbcc6e479bafc5e4b
alfonso mouton <alfonsomouton@gmail.com>
2025-04-04 21:55:21 -0300

    Añadido la gráfica de los índices GINI

--- Lab2/ASM_Interface/main.py
+++ Lab2/ASM_Interface/main.py
@@ -1,11 +1,32 @@
-import ctypes
 import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import ctypes
 import json
 
-response = requests.get("https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=%22Argentina%22")
-if response:
-    print("Response received successfully.")
+#https://documents.worldbank.org/en/publication/documents-reports/api
+
+response = requests.get(
+    "https://api.worldbank.org/v2/en/country/ARG/indicator/SI.POV.GINI?format=json&date=2000:2025"
+)
+
+if response.ok:
     data = response.json()
-    print(data[1][0])
+    results = data[1]  # El segundo elemento contiene los datos
+
+    year = []
+    value = []
+    for entry in results:
+        year.append(entry['date'])
+        value.append(entry['value'])
+    year = np.flip(np.array(year))
+    value = np.flip(np.array(value))
+
+    plt.plot(year, value, marker='o', label='Gini Index')
+    plt.title('Índice de Gini en Argentina (2011-2025)')
+    plt.xlabel('Año')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.show()
 else:
-    print("Failed to retrieve data.")
+    print("Error al obtener los datos:", response.status_code)
